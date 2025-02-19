44a8d93a01642361474d57b64df7a553fc83162d
Marco Acciarri <marco.acciarri@yourzone.it>
2025-02-19 18:16:22 +0100

    new block

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -3,6 +3,7 @@ import { types } from 'react-bricks/frontend'
 import HeroUnit from './custom/MyHeroUnit'
 import Pokemon from './custom/Pokemon'
 import Button from './custom/Button'
+import Button2 from './custom/Button2'
 import reactBricksUITheme from './react-bricks-ui'
 import Thumbnail from './custom/Thumbnail'
 
@@ -17,7 +18,7 @@ const bricks: types.Theme[] = [
       },
       {
         categoryName: 'Shared',
-        bricks: [Button], // Custom Bricks
+        bricks: [Button, Button2], // Custom Bricks
       },
       {
         categoryName: 'Pokemon',
