329549f75fa271bb125522eec64a09660dd9140f
Marco Acciarri <marco.acciarri@yourzone.it>
2025-02-25 12:20:32 +0100

    add alias to react-bricks-ui path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en'], // Add your languages here
     defaultLocale: 'en',
     localeDetection: false,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@reactbricksui'] = path.resolve(
+      __dirname,
+      'react-bricks/bricks/react-bricks-ui/'
+    )
+    return config
   },
 }
